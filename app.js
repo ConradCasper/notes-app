a9481d2d8bdb1a30a5d744bdd715e13f4dd1ddc1
Conrad <cnrdcasper@gmail.com>
2020-02-13 13:31:02 -0500

    using appendFileSync to append text to a txt file

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
-const fs = require('fs')
+const fs = require('fs');
 
-fs.writeFileSync('notes.txt', 'This file was created by Node.js!')
+fs.writeFileSync('notes.txt', 'My name is Conrad. ')
+
+fs.appendFileSync('notes.txt', 'Hi, nice to meet you, Conrad. My name is CatDog.')
