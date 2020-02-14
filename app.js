971441f12fe548113041401ec913372a11a480a2
Conrad <cnrdcasper@gmail.com>
2020-02-14 11:36:56 -0500

    created notes.js, created getNotes function. exported getNotes and required in app.js

--- app.js
+++ app.js
@@ -1,6 +1,32 @@
-const add = require('./utils')
+const getNotes = require('./notes')
 
-const sum = add(4, -2)
 
-console.log(sum)
+console.log(getNotes())
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const add = require('./utils')
+
+// const sum = add(4, -2)
+
+// console.log(sum)
---
+++ notes.js
@@ -0,0 +1,11 @@
+
+
+const getNotes = () => {
+    return "Your notes..."
+}
+
+
+
+
+
+module.exports = getNotes 
