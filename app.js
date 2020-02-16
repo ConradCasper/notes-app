5244fdf43c4e53279b35990431decf04ac864d91
Conrad <cnrdcasper@gmail.com>
2020-02-16 16:03:43 -0500

    using the yargs npm module

--- app.js
+++ app.js
@@ -1,15 +1,13 @@
 const chalk = require('chalk')
+const yargs = require('yargs')
 const getNotes = require('./notes')
 
-
 const log = console.log
-const command = process.argv[2]
 
-if (command === 'add'){
-    log('Adding note!')
-} else if (command === 'remove'){
-    console.log('Removing Note!')
-}
+
+
+log(process.argv)
+log(yargs.argv)
 
 
 
