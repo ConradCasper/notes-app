85a568d64a82edc8bc4f613f8980775a5a868761
Conrad <cnrdcasper@gmail.com>
2020-02-20 12:57:46 -0500

    read note function is working properly

--- notes.js
+++ notes.js
@@ -9,9 +9,10 @@ const getNotes = () => {
 
 const addNote = (title, body) => {
     const notes = loadNotes()
-    const duplicateNotes = notes.filter( note => note.title === title)
+    const duplicateNote = notes.find( note => note.title === title )
+    
 
-    if (duplicateNotes.length === 0) {
+    if (!duplicateNote) {
         notes.push({
             title: title,
             body: body
@@ -46,6 +47,19 @@ const listNotes = () => {
     } )
 }
 
+const readNote = (title) => {
+    const notes = loadNotes()
+    const note = notes.find( note => note.title === title )
+
+    if(note){
+        log(chalk.blue.bold.inverse(note.title))
+        log(note.body)
+    } else {
+        log(chalk.red.bold('No note found!'))
+    }
+
+}
+
 
 const saveNotes = (notes) => {
     const dataJSON = JSON.stringify(notes)
@@ -77,5 +91,6 @@ module.exports = {
     getNotes: getNotes,
     addNote: addNote,
     removeNote: removeNote,
-    listNotes: listNotes
+    listNotes: listNotes,
+    readNote: readNote
 }
